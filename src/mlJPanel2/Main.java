ecd394148890a5b59684815b7c93823f69d12105
MrGFHCI <scott.grondin@gmail.com>
2015-11-07 09:45:53 -0500

    Space3: shoot the enemy.

---
+++ src/mlJPanel2/Main.java
@@ -0,0 +1,16 @@
+package mlJPanel2;
+
+
+// this is an example of a basic program that does one thing:
+// click a button that updates a label.
+import mlJpanel.*;
+import Buttons2.*;
+import java.io.FileNotFoundException;
+
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        
+        FraMain fraMain = new FraMain();
+    }
+}
